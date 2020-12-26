5f9202b93a2cda99f0b52f272a216bee85e46748
ITÔ Hiroki <abies.firma@gmail.com>
2020-12-27 06:27:12 +0900

    update try-error in fit set R=1000 set seed for each model$sample

--- simulation.R
+++ simulation.R
@@ -6,11 +6,11 @@ library(cmdstanr)
 options(mc.cores = parallel::detectCores())
 library(posterior)
 
-# Seed of RNG
-set.seed(1)
+# Set seed of RNG
+set.seed(314)
 
 # Number of replications
-R <- 100
+R <- 1000
 
 # Function to generate rounded data
 round_n <- function(x, n) {
@@ -30,7 +30,7 @@ round_n <- function(x, n) {
 }
 
 # Function for the simulation
-sim <- function(N = 100, alpha = 2, sigma = 10) {
+sim_fun <- function(seed = 1, N = 100, alpha = 2, sigma = 10) {
   est <- rep(NA, 8)
 
   # Initial values of MCMC
@@ -43,96 +43,120 @@ sim <- function(N = 100, alpha = 2, sigma = 10) {
   D <- rweibull(N, alpha, sigma) # tree diameter data (cm)
   data1 <- list(N = N, D = D)
   model1 <- cmdstan_model("weibull.stan")
-  fit1 <- model1$sample(data = data1, init = inits,
+  fit1 <- model1$sample(data = data1, seed = seed, init = inits,
                         refresh = 0, show_messages = FALSE,
                         chains = 4,
                         iter_sampling = 2000, iter_warmup = 2000)
-  m <- fit1$summary(c("alpha", "sigma"), mean, rhat)
-  if (max(m$rhat) > 1.1)
-    stop("rhat > 1.1; ", m$rhat)
-  est[1:2] <- m$mean
-
+  m <- try(fit1$summary(c("alpha", "sigma"), mean, rhat))
+  if (class(m)[1] == "draws_summary") {
+    if (max(m$rhat) > 1.1)
+      stop("rhat > 1.1; ", m$rhat)
+    est[1:2] <- m$mean
+  }
+  
   # rounding to 0.1 cm
   D2 <- round(D, 1)
   data2 <- list(N = N, D = D2)
-  fit2 <- model1$sample(data = data2, init = inits,
+  fit2 <- model1$sample(data = data2, seed = seed, init = inits,
                         refresh = 0, show_messages = FALSE,
                         chains = 4,
                         iter_sampling = 2000, iter_warmup = 2000)
-  m <- fit2$summary(c("alpha", "sigma"), mean, rhat)
-  if (max(m$rhat) > 1.1)
-    stop("rhat > 1.1; ", m$rhat)
-  est[3:4] <- m$mean
+  m <- try(fit2$summary(c("alpha", "sigma"), mean, rhat))
+  if (class(m)[1] == "draws_summary") {
+    if (max(m$rhat) > 1.1)
+      stop("rhat > 1.1; ", m$rhat)
+    est[3:4] <- m$mean
+  }
 
   # rounding to 2 cm
   d3 <- round_n(D, 2)
   data3 <- list(K = d3$n_classes, B = d3$boundary, D = d3$y)
   model2 <- cmdstan_model("weibull_r.stan")
-  fit3 <- model2$sample(data = data3, init = inits,
+  fit3 <- model2$sample(data = data3, seed = seed, init = inits,
                         refresh = 0, show_messages = FALSE,
                         chains = 4,
                         iter_sampling = 2000, iter_warmup = 2000,
                         adapt_delta = 0.9)
-  m <- fit3$summary(c("alpha", "sigma"), mean, rhat)
-  if (max(m$rhat) > 1.1)
-    stop("rhat > 1.1; ", m$rhat)
-  est[5:6] <- m$mean
-
-  # rounding to 5 cm
+  m <- try(fit3$summary(c("alpha", "sigma"), mean, rhat))
+  if (class(m)[1] == "draws_summary") {
+    if (max(m$rhat) > 1.1)
+      stop("rhat > 1.1; ", m$rhat)
+    est[5:6] <- m$mean
+  }
+  
+    # rounding to 5 cm
   d4 <- round_n(D, 5)
   data4 <- list(K = d4$n_classes, B = d4$boundary, D = d4$y)
-  fit4 <- model2$sample(data = data4, init = inits,
+  fit4 <- model2$sample(data = data4, seed = seed, init = inits,
                         refresh = 0, show_messages = FALSE,
                         chains = 4,
                         iter_sampling = 2000, iter_warmup = 2000,
                         adapt_delta = 0.98)
-  m <- fit4$summary(c("alpha", "sigma"), mean, rhat)
-  if (max(m$rhat) > 1.1)
-    stop("rhat > 1.1; ", m$rhat)
-  est[7:8] <- m$mean
+  m <- try(fit4$summary(c("alpha", "sigma"), mean, rhat))
+  if (class(m)[1] == "draws_summary") {
+    if (max(m$rhat) > 1.1)
+      stop("rhat > 1.1; ", m$rhat)
+    est[7:8] <- m$mean
+  }
 
   return(est)
 }
 
+
+sim <- vector("list", 6)
+
 # Simulation 1
 # N = 30, alpha = 2, sigma = 8
-print("simulation 1")
-sim1 <- replicate(R, sim(N = 30, alpha = 2, sigma = 8))
-apply(sim1, 1, mean)
-apply(sim1, 1, sd)
+sim[[1]] <- sapply(1:R,
+                   function(i)
+                     sim_fun(i, N = 30, alpha = 2, sigma = 8))
+apply(!is.na(sim[[1]]), 1, sum)
+apply(sim[[1]], 1, mean, na.rm = TRUE)
+apply(sim[[1]], 1, sd, na.rm = TRUE)
 
 # Simulation 2
 # N = 300, alpha = 2, sigma = 8
-print("simulation 2")
-sim2 <- replicate(R, sim(N = 300, alpha = 2, sigma = 8))
-apply(sim2, 1, mean)
-apply(sim2, 1, sd)
+sim[[2]] <- sapply(1:R,
+                   function(i)
+                     sim_fun(i, N = 300, alpha = 2, sigma = 8))
+apply(!is.na(sim[[2]]), 1, sum)
+apply(sim[[2]], 1, mean, na.rm = TRUE)
+apply(sim[[2]], 1, sd, na.rm = TRUE)
 
 # Simulation 3
-# N = 30, alpha = 4, sigma = 15
-print("simulation 3")
-sim3 <- replicate(R, sim(N = 30, alpha = 4, sigma = 12))
-apply(sim3, 1, mean)
-apply(sim3, 1, sd)
+# N = 30, alpha = 4, sigma = 12
+sim[[3]] <- sapply(1:R,
+                   function(i)
+                     sim_fun(i, N = 30, alpha = 4, sigma = 12))
+apply(!is.na(sim[[3]]), 1, sum)
+apply(sim[[3]], 1, mean, na.rm = TRUE)
+apply(sim[[3]], 1, sd, na.rm = TRUE)
 
 # Simulation 4
-# N = 300, alpha = 4, sigma = 15
-print("simulation 4")
-sim4 <- replicate(R, sim(N = 300, alpha = 4, sigma = 12))
-apply(sim4, 1, mean)
-apply(sim4, 1, sd)
+# N = 300, alpha = 4, sigma = 12
+sim[[4]] <- sapply(1:R,
+                   function(i)
+                     sim_fun(i, N = 300, alpha = 4, sigma = 12))
+apply(!is.na(sim[[4]]), 1, sum)
+apply(sim[[4]], 1, mean, na.rm = TRUE)
+apply(sim[[4]], 1, sd, na.rm = TRUE)
 
 # Simulation 5
 # N = 30, alpha = 5, sigma = 20
-print("simulation 3")
-sim3 <- replicate(R, sim(N = 30, alpha = 5, sigma = 20))
-apply(sim3, 1, mean)
-apply(sim3, 1, sd)
+sim[[5]] <- sapply(1:R,
+                   function(i)
+                     sim_fun(i, N = 30, alpha = 5, sigma = 20))
+apply(!is.na(sim[[5]]), 1, sum)
+apply(sim[[5]], 1, mean, na.rm = TRUE)
+apply(sim[[5]], 1, sd, na.rm = TRUE)
 
 # Simulation 6
 # N = 300, alpha = 5, sigma = 20
-print("simulation 4")
-sim4 <- replicate(R, sim(N = 300, alpha = 5, sigma = 20))
-apply(sim4, 1, mean)
-apply(sim4, 1, sd)
+sim[[6]] <- sapply(1:R,
+                   function(i)
+                     sim_fun(i, N = 300, alpha = 5, sigma = 20))
+apply(!is.na(sim[[6]]), 1, sum)
+apply(sim[[6]], 1, mean, na.rm = TRUE)
+apply(sim[[6]], 1, sd, na.rm = TRUE)
 
+save(sim, file = "sim_results.RData")
