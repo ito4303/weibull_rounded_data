e8aa972ce56bd5a66b3143a0537fcd41e8dda60c
ITÔ, Hiroki <hiroki@affrc.go.jp>
2021-02-26 10:18:25 +0900

    update results.R

--- results.R
+++ results.R
@@ -1,10 +1,12 @@
 library(dplyr)
 library(tibble)
 library(tidyr)
+library(stringr)
 
 load("sim_results.RData")
 
 n_sim <- 6
+n_type <- 4
 
 n <- sapply(seq_len(n_sim), function(i)
   apply(!is.na(sim[[i]]), 1, sum))
@@ -12,45 +14,44 @@ m <- sapply(seq_len(n_sim), function(i)
   apply(sim[[i]], 1, mean, na.rm = TRUE))
 s <- sapply(seq_len(n_sim), function(i)
   apply(sim[[i]], 1, sd, na.rm = TRUE))
-data_type <- rep(1:4, each = 2)
-results <- tibble(simulation = rep(seq_len(n_sim), each = 8),
-                  data_type = rep(data_type, n_sim),
-                  var = rep(c("alpha", "sigma"), n_sim * 4),
+data_type <- rep(seq_len(n_type), each = 2)
+results <- tibble(sim = rep(seq_len(n_sim), each = n_type * 2),
+                  type = rep(data_type, n_sim),
+                  var = rep(c("alpha", "sigma"), n_sim * n_type),
                   num = c(n),
                   mean = c(m),
                   sd = c(s))
 
-tbl_alpha <- results %>%
-  dplyr::filter(var == "alpha")
-tbl_sigma <- results %>%
-  dplyr::filter(var == "sigma")
+# function to summarize the results
+result_tbl <- function(results, var_name = "alpha") {
+  results %>%
+    dplyr::filter(var == var_name) %>%
+    dplyr::transmute(
+      sim, type,
+      est = str_c(sprintf("%2.1f", mean),
+                  "±",
+                  sprintf("%2.1f", sd))) %>%
+    tidyr::pivot_wider(names_from = type,
+                       names_prefix = "data_type",
+                       values_from = est)
+}
+
+# alpha
+alpha_tbl <- results %>%
+  result_tbl("alpha")
+print(alpha_tbl)
+write.csv(alpha_tbl, "simulation_alpha.csv")
+
+# sigma
+sigma_tbl <-  results %>%
+  result_tbl("sigma")
+print(sigma_tbl)
+write.csv(sigma_tbl, "simulation_sigma.csv")
 
 # n
-tbl_alpha %>%
-  dplyr::select(simulation, data_type, num) %>%
-  tidyr::pivot_wider(names_from = data_type,
-                     values_from = num)
-
-# alpha: mean
-tbl_alpha %>%
-  tidyr::pivot_wider(names_from = data_type,
-                     values_from = c(num, mean, sd)) %>%
-  dplyr::select(starts_with("mean"))
-
-# alpha: sd
-tbl_alpha %>%
-  tidyr::pivot_wider(names_from = data_type,
-                     values_from = c(num, mean, sd)) %>%
-  dplyr::select(starts_with("sd"))
-
-# sigma: mean
-tbl_sigma %>%
-  tidyr::pivot_wider(names_from = data_type,
-                     values_from = c(num, mean, sd)) %>%
-  dplyr::select(starts_with("mean"))
-
-# sigma: sd
-tbl_sigma %>%
-  tidyr::pivot_wider(names_from = data_type,
-                     values_from = c(num, mean, sd)) %>%
-  dplyr::select(starts_with("sd"))
+results %>%
+  dplyr::select(sim, type, var, num) %>%
+  dplyr::filter(var == "alpha") %>%
+  tidyr::pivot_wider(names_from = type,
+                     values_from = num) %>%
+  dplyr::select(-var)
